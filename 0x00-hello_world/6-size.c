da57401cf88cf508c47153ed710b715806abbeec
HamzaChebaane <hamzachbaane@gmail.com>
2022-12-08 16:23:06 -0800

    Write a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int e;
 	float f;
 	
-	printf("The size of a char is: %lu \n", (unsigned long)sizeof(c));
-	printf("The size of an int is: %lu \n", (unsigned long)sizeof(i));
-	printf("The size of a long int is: %ly \n", (unsigned long)sizeof(d));
-	printf("The size of a long long int is: %lu \n", (unsigned long)sizeof(e));
-	printf("The size of a float is: %lu \n", (unsigned long)sizeof(f));
+	printf("The size of a char: %lu \n", (unsigned long)sizeof(c));
+	printf("The size of an int: %lu \n", (unsigned long)sizeof(i));
+	printf("The size of a long int: %ly \n", (unsigned long)sizeof(d));
+	printf("The size of a long long int: %lu \n", (unsigned long)sizeof(e));
+	printf("The size of a float: %lu \n", (unsigned long)sizeof(f));
 
 
 	return (0);
