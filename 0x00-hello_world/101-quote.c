f754c1e9c52b23f355be8ebe066dfbaa0d40209f
HamzaChebaane <hamzachbaane@gmail.com>
2022-12-08 16:59:38 -0800

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
