c7700983202b00b7974c77f38f938ea331115333
HamzaChebaane <hamzachbaane@gmail.com>
2022-12-20 15:26:33 -0800

    copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,8 +4,8 @@
  * *_strcpy - opies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest
- * @dest: 
- * @src:
+ * @dest: destiny 
+ * @src: copia
  * Return: the pointer to dest
  */
 
