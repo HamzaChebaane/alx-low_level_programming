0b9fb6df7c92ba2eb360c0588786f1ad8aeb92eb
HamzaChebaane <hamzachbaane@gmail.com>
2023-01-01 11:39:04 -0800

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
 
 /**
  * main - Enter point
@@ -8,7 +7,8 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"),
+1, stderr);
 	return (1);
 }
