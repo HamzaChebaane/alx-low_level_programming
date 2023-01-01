4cf8995e8e18d89c4805c18e2b0ccbc6e3f494c6
HamzaChebaane <hamzachbaane@gmail.com>
2023-01-01 11:12:02 -0800

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		       	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), 1, stderr);
+		       sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),
+		       1, stderr);
 	return (1);
 }
