14ab05906a68d7887167cda73934c1214226101e
HamzaChebaane <hamzachbaane@gmail.com>
2023-01-16 13:30:48 -0800

    a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,3 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog - rename strucut with typedef
- */
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
