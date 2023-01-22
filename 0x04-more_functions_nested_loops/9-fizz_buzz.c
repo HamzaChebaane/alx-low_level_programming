ad4e7dcf6f17f31697aa89b68fd34a8b58b72ce4
HamzaChebaane <hamzachbaane@gmail.com>
2023-01-22 09:28:10 -0800

    The “Fizz-Buzz test” is an interview question

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - The “Fizz-Buzz test” interview
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 15) = 0)
+		{
+			_putchar(' ');
+			printf(FizzBuzz);
+		}
+		else if ((i % 5) = 0)
+		{
+			_putchar(' ');
+			printf(Buzz);
+		}
+		else if ((i % 3) = 0)
+		{
+			_putchar(' ');
+			printf(Fizz);
+		}
+		else if (i != 0)
+		{
+			_putchar(' ');
+			_putchar(i + '0');
+		}
+		else
+		{
+			_putchar(48);
+		}
+	}
+}
