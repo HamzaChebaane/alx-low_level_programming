cbb53404e46e4b003cba46112fd30c3347819198
HamzaChebaane <hamzachbaane@gmail.com>
2023-02-06 14:26:39 -0800

    a function that tests if the input integer is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,31 @@
 #include "main.h"
 
 /**
- *
+ * prime1 - evaluate from 1 to n
+ * @a: same as n
+ * @b: iterate from 1 to n
+ * Return: On success 1
+ * 0 otherwise
+ */
+
+int prime1(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime1(a, b + 1));
+}
+/**
+ * is_prime_number - tests if the input integer is a prime number
+ * @n: int to bes tested
+ * Return: On success 1
+ * 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime1(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,3 +7,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
 int sqrt1(int a, int b);
+int prime1(int a, int b);
